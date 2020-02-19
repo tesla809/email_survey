e55cfe717362590664b61dbc48c3cb24953eb88b
anthony.albertorio <anthony.albertorio@gmail.com>
2020-02-19 14:17:59 -0500

    added configuration to use cookies with authentication process in index.js

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');  // commonJS modules
-const mongoose = require('mongoose');
+const mongoose = require('mongoose');  // to interact with MongoDB database
+const cookieSession = require('cookie-session');  // handles cookies since express doesn't
+const passport = require('passport');  // middleware helper to deal with authentication with various providers.
 const authRoutes = require('./routes/authRoutes');
-const keys = require('./config/keys');
+const apiRoutes = require('./routes/apiRoutes');
+const keys = require('./config/keys');  // holds our secret keys we don't commit.
 
 // load models first
 require('./models/User');
@@ -23,6 +26,17 @@ try {
 
 const app = express();  // setup server instance
 
+// cookie config and setup with passport
+app.use(  // set up cookies
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,  // expiration date in 30 days,
+    keys: [keys.cookieKey] // automatic encryption of key for cookie, can use multiple keys in array for security
+  })
+);
+
+app.use(passport.initialize())  // start passport session?
+app.use(passport.session());  // get passport to use cookies?
+
 // routes
 app.get('/', (req, res) => {  // root route
   res.send('Home page');
@@ -30,6 +44,7 @@ app.get('/', (req, res) => {  // root route
 
 passportConfig(); // config passport OAuth- Google
 app.use('/auth', authRoutes);
+app.use('/api', apiRoutes);
 
 // set up ports
 const PORT = process.env.PORT || 5000;  // dynamic port binding for Heroku. Heroku injects enviroment variables to app via some runtime configuration. Or use Port 5000 if no .env exists 
