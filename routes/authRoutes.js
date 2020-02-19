9089a54680f407154cef9237ad11bcb0062282e7
anthony.albertorio <anthony.albertorio@gmail.com>
2020-02-19 14:17:01 -0500

    added endpoint to logout of application in routes/authRoutes.js.

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();  // modularize our routes
 const passport = require('passport');  // require the original passportJS module
 
-// main auth entrance.
+// auth main entrance.
 router.get('/', (req, res) => {
-  res.send('Go to .../auth/google');
+  res.send('Auth routes main. Go to .../auth/google');
 });
 
 // google auth. Note that if /auth is used in index.js, its kicked over here to use
@@ -17,5 +17,11 @@ router.get('/google',  // route to handle google auth
 router.get('/google/callback', // code (inside returned url) sent back by google, to give app permission to data, is handled by passport. Make sure that callback address is whitelisted on google's OAuth service
   passport.authenticate('google')
 );
+
+router.get('/logout', (req, res) => {
+  req.logout();  // passport attaches .logout() automatically to req. Kills cookie id.
+  res.send(`Goodbye! Signed out! req.user is ${req.user}.`);
+});
+
 
 module.exports = router;
