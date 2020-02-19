50f5afa89bcdd72e1a4ec54329ccadfbe20711f5
anthony.albertorio <anthony.albertorio@gmail.com>
2020-02-19 14:20:59 -0500

    added code to save user to mongoDB data base only if they are unique, and to create/read cookies once user is signed in.

--- services/passport.js
+++ services/passport.js
@@ -6,28 +6,26 @@ const mongoose = require('mongoose');  // loaded like this to avoid confusing mo
 const User = mongoose.model('User');  // already loaded into mongoose in User.js, so we can access it.
 
 passport.serializeUser((user, done) => {  // create cookie from user model
-  done(null, user.id);  // user.id is primary key made by mongoDB, not profile.id. Ids unique user in database. After user signs in we only care about database id. Google id is for auth flow only
+  done(null, user.id);  // user.id is primary key made by mongoDB, not profile.id. Ids unique user in database. After user signs in we only care about database id. Google id is for auth flow only.
 });
 
 passport.deserializeUser((id, done) => {  // turn cookie to mongoose model user instance
   User.findById(id)  // find user by id
     .then(user => {  // async, so handle promise 
-      done(null, user);
+      done(null, user);  // done() is passportJS middleware to tell it to move to next step in auth. null is passed in as an error handler
     })  
 });
-
-
 
 const saveToDatabase = (accessToken, refreshToken, profile, done) => {  // in this callback, we can create new user in database
   User.findOne({ googleId: profile.id }) // query to see if googleId matches profileId. If no, create.
     .then(existingUser => {
-      if (existingUser) {  // if exisitingUser exist.
+      if (existingUser) {  // if exisitingUser exist, don't add them again.
         done(null, existingUser); // done(err, userRecord)tell passportJS -> finished, proceed w/ auth flow, no error, here is user.
         console.log(`${existingUser}User exists already`)
       } else {
         new User({ googleId: profile.id })  // user info  we send to database
           .save()  // save our ID into our database -> ourId: google's id
-          .then(newUser => done(null, newUser));  // userInfo database sends back to us. Might have more info
+          .then(newUser => done(null, newUser));  // userInfo database sends back to us. Might have more info, including the primary key that MongoDB adds to each new record.
         console.log(`${profile.id} SAVED`);
       }
     });
