6bc257381427a8ac567866353018106fe670c351
anthony.albertorio <anthony.albertorio@gmail.com>
2020-02-20 16:08:04 -0500

    altered config/prod.js to use process.env values instead of hard coded valued. Added values on heroku account.

--- config/prod.js
+++ config/prod.js
@@ -7,17 +7,3 @@ module.exports = {
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY
 }
-
-/*
-const mongoDB = {
-  adminUserName: 'Prod-Brainiac',
-  adminPassword: 'Vg321EPMvTJKPmUO'
-}
-
-module.exports = {
-  googleClientID: '496820388351-vavp0sgaef8cinsteb71sg0p46pac2vh.apps.googleusercontent.com',
-  googleClientSecret: 'ZaiBqzk7nBvCjxWPNWOEM2RK',
-  mongoURI: `mongodb+srv://${mongoDB.adminUserName}:${mongoDB.adminPassword}@cluster0-fhgyx.mongodb.net/test?retryWrites=true&w=majority`,
-  cookieKey: 'zIaotRw9zE3u8zYZRi85kz6glYswE1Wb6KJxnfDKr0l0xOv9pEQN2HHefXJK'  // can be any string of characters
-}
-*/
