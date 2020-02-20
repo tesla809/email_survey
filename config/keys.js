fcd0aae174acda1962776517713e8c30705c7a26
anthony.albertorio <anthony.albertorio@gmail.com>
2020-02-20 16:11:48 -0500

    changed typo in code. From Node_ENV to NODE_ENV.

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 // keys.js - figure out what set of credentials to return
 // prod or dev
 
-module.exports =  process.env.Node_ENV  === 'production'
+module.exports =  process.env.NODE_ENV === 'production'
   ? require('./prod') // if production, return prod keys
   : require('./dev'); // if dev, return dev keys
