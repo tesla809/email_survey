cc837128ea7c194420b6931072fc53bacbee0169
anthony.albertorio <anthony.albertorio@gmail.com>
2020-02-29 16:07:02 -0500

    Set up React Router with dummy components to match needed routes.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const Header = () => <h2>Header</h2>
+const Dashboard = () => <h2>Dashboard</h2>
+const SurveyNew = () => <h2>SurveyNew</h2>
+const Landing = () => <h2>Landing</h2>
 
 const App = () => {
   return (
-    <div>Hi There!</div>
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/surveys" component={Dashboard} />
+        <Route exact path="/surveys/new" component={SurveyNew} />
+      </BrowserRouter>
+    </div>
   );
 };
 
 export default App;
+
+/*
+BrowserRouter
+Brains of React Router.
+Looks are url and changes components 
+visible on screen based on it.
+
+Route
+Actually, sets up rule between route and set of components
+*/
