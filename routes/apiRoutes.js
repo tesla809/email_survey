1bcb93ed0b65e4d14478c11650b98938ce66f095
anthony.albertorio <anthony.albertorio@gmail.com>
2020-02-29 17:15:32 -0500

    changed .send() of /current_user in routes/apiRoutes.js to send req.user only.
    Previously had a string sent.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -10,7 +10,7 @@ router.get('/', (req, res) => {
 router.get('/current_user', (req, res) => {  // If req.user exists, auth works.
   console.log(req.session);  // contains data passport is trying to store in cookie. Cookie-session(extract cookie data) -> assigns to req.session -> inside req.sesson passport key exists -> passport(pull user id out of cookie data) -> deserializeUser(userId to user instance)
   console.log(req.user);  // when we sign in, passportJS adds user to request object.
-  res.send(`req.user is: ${req.user}. req.session is ${req.session}`); // send user. Make sure that you sign in with /auth/google first
+  res.send(req.user); // send user. Make sure that you sign in with /auth/google first
 });
 
 module.exports = router;
