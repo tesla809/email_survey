fd78961097e343023818b5867fac686e18ab2b10
anthony.albertorio <anthony.albertorio@gmail.com>
2020-03-01 18:58:58 -0500

    added redux store and redux-thunk middleware to index.js in client/.

--- client/src/index.js
+++ client/src/index.js
@@ -4,14 +4,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';  // manages react redux integeration. Provider reads/passes store to App and all its components
 import { createStore, applyMiddleware } from 'redux'; 
+import reduxThunk from 'redux-thunk';
 
 import App from './components/App';
 import reducers from './reducers';  // will import index.js
 
+
+
+
 const store = createStore(  // create new instance of redux store
   reducers,                 // reducers to pass into state
   {},                       // initial state of app, populate if you want server side rendering
-  applyMiddleware()         // add redux-thunk here
+  applyMiddleware(          // add redux-thunk here
+    reduxThunk
+  )
 );
 
 ReactDOM.render(
