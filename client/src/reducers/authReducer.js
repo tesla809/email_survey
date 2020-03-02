bb5327adb0025355608c959ee44206521fc0a530
anthony.albertorio <anthony.albertorio@gmail.com>
2020-03-02 15:31:55 -0500

    refactored code in client/src/reducers/authReducer.js to handle actions with FETCH_USER and authentication.

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,6 +1,9 @@
-export default function(state = {}, action) {
-  console.log(action);
+import { FETCH_USER } from '../actions/types';
+
+export default function(state = null, action) {  // null is not signed in by default
   switch (action.type) {
+    case FETCH_USER:
+      return action.payload || false;  // user model or false (not signed in, payload is empty string). Empty string is falsy value.
     default:
       return state
   }
