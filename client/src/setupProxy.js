b58bc36388c3b5e1322124dcbf062be67a69e696
anthony.albertorio <anthony.albertorio@gmail.com>
2020-03-03 12:13:39 -0500

    added /auth/logout to client/src/setupProxy.js.

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -3,7 +3,7 @@ const proxy = require('http-proxy-middleware');
 module.exports = function(app) {
   app.use(
     proxy(
-      ['/api', '/auth/google'], 
+      ['/api', '/auth/google', '/auth/logout'], 
       { target: 'http://localhost:5000' })
   );
 }
