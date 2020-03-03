7b501ec615180845b08654ceb1acd88e5a053185
anthony.albertorio <anthony.albertorio@gmail.com>
2020-03-03 12:15:27 -0500

    added logic to show sign with google or logout to Header.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,18 +1,34 @@
 import React, { Component } from "react";
+import { connect } from 'react-redux';
 
 class Header extends Component {
- render() {
-   return(
-     <nav>
-      <div className="nav-wrapper">
-        <a className="left brand-logo">Emaily</a>
-        <ul className="right">
-          <li><a>Login with Google</a></li>
-        </ul>
-      </div>
-     </nav>
-   );
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return <li><a href="/auth/google">Login With Google</a></li>
+      default:  // must be logged in
+        return <li><a href="/auth/logout">Logout</a></li>
+    }
+  }
+
+  render() {
+    return(
+      <nav>
+        <div className="nav-wrapper">
+          <a className="left brand-logo">Emaily</a>
+          <ul className="right">
+            <li>{this.renderContent()}</li>
+          </ul>
+        </div>
+      </nav>
+    );
  } 
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
